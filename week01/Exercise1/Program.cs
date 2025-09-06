db4293337410671068ff4cd771f829f356c21a4d
Brian Rios <r4y4n.4j.z3r1ck@gmail.com>
2025-09-06 14:06:53 -0500

    first excercise done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// -------------------------------------------------------
+// MODULO 1 : Imports
+// -------------------------------------------------------
+
+using System;
+
+Console.WriteLine("Proving things");
+
+string name = Ask("What is your first name? ");
+string last_name = Ask("What is your last name? ");
+
+Console.WriteLine($"Your name is {last_name}, {name} {last_name}");
+
+// -------------------------------------------------------
+// MODULO 2 : Function
+// -------------------------------------------------------
+
+static string Ask(string prompt)
+{
+    Console.Write(prompt);
+    var message = Console.ReadLine();
+    return string.IsNullOrWhiteSpace(message) ? "" : message.Trim();
+};
